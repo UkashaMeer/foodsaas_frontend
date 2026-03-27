dd065e7fa718e27d4482536b417fa812f08516f2
UkashaMeer <156102873+UkashaMeer@users.noreply.github.com>
2026-03-27 23:48:45 +0500

    Change axios baseURL from HTTP to HTTPS

--- src/lib/axiosClient.jsx
+++ src/lib/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://foodsaas-backend-aoyrcg-0e6a59-151-243-3-122.traefik.me/api"
+    baseURL: "https://foodsaas-backend-aoyrcg-0e6a59-151-243-3-122.traefik.me/api"
 })
 
 axiosClient.interceptors.request.use((config) => {
